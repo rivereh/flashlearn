96ff76dcd3182089deb8aaf568d7a3c93fd99316
River <riverehill@gmail.com>
2023-10-27 02:23:55 -0700

    Change api url

--- src/hooks/useSignup.jsx
+++ src/hooks/useSignup.jsx
@@ -10,11 +10,14 @@ export const useSignup = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('http://54.214.110.106:4000/api/user/signup', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password }),
-    })
+    const response = await fetch(
+      'http://54.214.110.106:4000/api/user/signup/',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      }
+    )
     const json = await response.json()
 
     if (!response.ok) {
